17258cc216e46bddbf485a237c92384fc61c97be
KierstonHill83 <khill1218@gmail.com>
2015-10-15 17:14:25 -0600

    Added partials, controllers, and css

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'homeController',
+      templateUrl: '/views/partials/home.html',
+      access:{restricted: false},
+    })
+    .when('/bio', {
+      controller: 'bioController',
+      templateUrl: '/views/partials/bio.html',
+      access:{restricted: false},
+    })
+    .when('/contact', {
+      controller: 'contactController',
+      templateUrl: '/views/partials/contact.html',
+      access:{restricted: false},
+    })
+    .when('/resume', {
+      controller: 'resumeController',
+      templateUrl: '/views/partials/resume.html',
+      access:{restricted: false},
+    })
+    .when('/portfolio', {
+      controller: 'portfolioController',
+      templateUrl: '/views/partials/portfolio.html',
+      access:{restricted: false},
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
